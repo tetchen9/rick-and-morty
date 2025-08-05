80d958a8103d96fed1acc8dfcff2003019da4969
Tania <tania@Tanias-MacBook.local>
2025-08-05 12:42:12 +0800

    characters path redirect

--- next.config.ts
+++ next.config.ts
@@ -19,7 +19,16 @@ const nextConfig: NextConfig = {
         hostname: 'rickandmortyapi.com',
       },
     ],
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/characters', 
+        destination: '/characters/1', 
+        permanent: true,
+      },
+    ];
+  },
 }
 
 export default nextConfig
